265b1c91be0a16e3a74b02a18170febcba8c6039
Ryan Cook <80371197+ryanc410@users.noreply.github.com>
2021-10-03 01:35:57 -0500

    10.03.2021
    Fixed Script

--- nextcloud.sh
+++ nextcloud.sh
@@ -1,180 +1,165 @@
-#!/bin/bash
-
-
-########################
+#!/usr/bin/env bash
+###########################################################################
+# Author: RYAN COOK
+# Version: v1.0.2
+# Date: 2021-10-03
+# Description: Nextcloud Auto Install Script
+# Usage: ./nextcloud.sh
+###########################################################################
 # VARIABLES
-########################
+###########################################################################
+DOMAIN=''
+DB_ROOT_PW=''
+DB_USER=''
+DB_USER_PASS=''
 
-ip=$(curl ifconfig.me.)
-
-# Fill in below this line only
-
-MYSQL_ROOT_PW=
-MYSQL_USER=
-MYSQL_USER_PW=
-nextcloud_user=
-ncuser_pass=
-
-########################
+###########################################################################
 # FUNCTIONS
-########################
-
+###########################################################################
+checkroot()
+{
+  if [[ $EUID != 0 ]]; then
+    echo "Must be root to run this script!"
+    sleep 2
+    exit 1
+  fi
+}
 check_os()
 {
-    if [[ $OSTYPE != linux-gnu ]]; then
-        title
-        echo "Script is not compatible with your OS."
-        sleep 3
-        exit 1
-    fi
+	if [[ $OSTYPE != linux-gnu ]]; then
+	  echo "Script not compatible with your Operating system!"
+	  sleep 2
+	  exit 2
+	fi
 }
-pub_ip()
+check_args()
 {
-    command -v curl &>/dev/null
-        if [[ $? -ne 0 ]]; then
-            sudo apt install curl -y &>/dev/null
-        fi
+  if [[ $DOMAIN = '' ]]; then
+    DOMAIN=$(hostname -f)
+    echo "--Nextcloud Auto Install Script--">>POST_INSTALL_NOTES.txt
+    echo "Nextcloud Domain: $DOMAIN">>POST_INSTALL_NOTES.txt
+  fi
+  if [[ $DB_ROOT_PW = '' ]]; then
+    DB_ROOT_PW=$(openssl rand -base64 32)
+    echo "DB Root User Password: $DB_ROOT_PW">>POST_INSTALL_NOTES.txt
+  fi
+  if [[ $DB_USER = '' ]]; then
+    DB_USER=nextcloud_user
+    echo "Nextcloud Database User Name: $DB_USER">>POST_INSTALL_NOTES.txt
+  fi
+  if [[ $DB_USER_PASS = '' ]]; then
+    DB_USER_PASS=$(openssl rand -base64 20)
+    echo "$DB_USER Password: $DB_USER_PASS">>POST_INSTALL_NOTES.txt
+  fi
 }
-title()
-{
-    clear
-    echo "#########################################"
-    echo "#    Nextcloud Auto Installer Script    #"
-    echo "#########################################"
-    echo ""
-}
+
+###########################################################################
+# SCRIPT
+###########################################################################
+checkroot
 
 check_os
 
-pub_ip
+check_args
 
-title
+apt update && apt upgrade -y
 
-# Update the server
-sudo apt update && sudo apt upgrade -y
+apt install apache2 apache2-utils -y
 
-# Install apache2
-sudo apt install apache2 apache2-utils -y
+systemctl start apache2 && systemctl enable apache2
 
-sudo systemctl start apache2 && sudo systemctl enable apache2
+echo "ServerName localhost">>/etc/apache2/conf-available/servername.conf
 
-# Set Web Root permissions
-sudo chown www-data:www-data /var/www/html/ -R
+a2enconf servername.conf
 
-# Create Servername.conf and enable it
-sudo echo "ServerName localhost">>/etc/apache2/conf-available/servername.conf
+systemctl reload apache2
 
-sudo a2enconf servername.conf
+apt install mariadb-server mariadb-client -y
 
-sudo systemctl reload apache2
+systemctl start mariadb && systemctl enable mariadb
 
-# Install Mariadb server
-sudo apt install mariadb-server mariadb-client -y
-
-# Start and enable mariadb server
-sudo systemctl start mariadb && sudo systemctl enable mariadb
-
-# Mysql secure installation
-sudo mysql << BASH_QUERY
-SET PASSWORD FOR root@localhost = PASSWORD('$MYSQL_ROOT_PW');
+mysql << BASH_QUERY
+SET PASSWORD FOR root@localhost = PASSWORD('$DB_ROOT_PW');
 FLUSH PRIVILEGES;
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
-CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_USER_PW';
-GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost';FLUSH PRIVILEGES;
+CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_USER_PASS';
+CREATE DATABASE nextcloud;
+GRANT ALL PRIVILEGES ON nextcloud.* TO '$DB_USER'@'localhost';
+FLUSH PRIVILEGES;
 BASH_QUERY
 
-sudo apt install php7.4 libapache2-mod-php7.4 php7.4-mysql php-common php7.4-cli php7.4-common php7.4-json php7.4-opcache php7.4-readline -y
+apt install php7.4-{mysql,cli,common,json,opcache,readline} php-common libapache2-mod-php7.4 -y
 
-sudo a2dismod php7.4
+a2dismod php7.4
 
-sudo apt install php7.4-fpm -y
+apt install php7.4-fpm -y
 
-sudo a2enmod proxy_fcgi setenvif
+a2enmod proxy_fcgi setenvif
 
-sudo a2enconf php7.4-fpm
+a2enconf php7.4-fpm
 
-sudo systemctl restart apache2
+systemctl restart apache2
 
-wget https://download.nextcloud.com/server/releases/nextcloud-22.1.0.zip
+wget https://download.nextcloud.com/server/releases/nextcloud-22.2.0.zip
 
-sudo apt install unzip -y
+apt install unzip -y
 
-sudo unzip nextcloud-22.1.0.zip -d /var/www/
+unzip nextcloud-**.*.*.zip -d /var/www/
 
-sudo chown www-data:www-data /var/www/nextcloud/ -R
-
-sudo mysql<< _QUERY
-create database nextcloud;
-create user $nextcloud_user@localhost identified by '$ncuser_pass';
-grant all privileges on nextcloud.* to $nextcloud_user@localhost identified by '$ncuser_pass';
-flush privileges;
-exit;
-_QUERY
+chown www-data:www-data /var/www -R
 
 cat << _EOF_ >> /etc/apache2/sites-available/nextcloud.conf
-<VirtualHost $ip:80>
+<VirtualHost *:80>
         DocumentRoot "/var/www/nextcloud"
-        ServerName $domain
-
+        ServerName $DOMAIN
         ErrorLog ${APACHE_LOG_DIR}/nextcloud.error
         CustomLog ${APACHE_LOG_DIR}/nextcloud.access combined
-
         <Directory /var/www/nextcloud/>
             Require all granted
             Options FollowSymlinks MultiViews
             AllowOverride All
-
            <IfModule mod_dav.c>
                Dav off
            </IfModule>
-
         SetEnv HOME /var/www/nextcloud
         SetEnv HTTP_HOME /var/www/nextcloud
         Satisfy Any
-
        </Directory>
-
 </VirtualHost>
 _EOF_
 
-sudo a2ensite nextcloud.conf
+a2ensite nextcloud.conf
 
-sudo a2enmod rewrite headers env dir mime setenvif ssl
+a2enmod rewrite headers env dir mime setenvif ssl
 
-sudo systemctl restart apache2
+systemctl restart apache2
 
-sudo apt install imagemagick php-imagick libapache2-mod-php7.4 php7.4-common php7.4-mysql php7.4-fpm php7.4-gd php7.4-json php7.4-curl php7.4-zip php7.4-xml php7.4-mbstring php7.4-bz2 php7.4-intl php7.4-bcmath php7.4-gmp -y
+apt install certbot python3-certbot-apache -y
 
-sudo systemctl reload apache2
+certbot --apache --agree-tos --redirect --staple-ocsp --email admin@"$DOMAIN" -d "$DOMAIN"
 
-sudo apt install certbot python3-certbot-apache -y
+sed -i 's/memory_limit = 128M/memory_limit = 512M/g' /etc/php/7.4/apache2/php.ini
 
-sudo certbot --apache --agree-tos --redirect --staple-ocsp --email admin@$domain -d $domain
+sed -i 's/memory_limit = 128M/memory_limit = 512M/g' /etc/php/7.4/fpm/php.ini
 
-sudo sed -i 's/memory_limit = 128M/memory_limit = 512M/g' /etc/php/7.4/apache2/php.ini
+systemctl reload apache2
 
-sudo systemctl reload apache2
+apt install redis-server -y
 
-sudo sed -i 's/memory_limit = 128M/memory_limit = 512M/g' /etc/php/7.4/fpm/php.ini
+systemctl start redis-server && systemctl enable redis-server
 
-sudo systemctl reload php7.4-fpm
+apt install php-redis -y
 
-sudo apt install redis-server -y
+phpenmod redis
 
-sudo systemctl start redis-server && sudo systemctl enable redis-server
-
-sudo apt install php-redis -y
-
-sudo phpenmod redis
-
-sudo systemctl reload apache2
+systemctl reload apache2
 
 sed -i '$ d' /var/www/nextcloud/config/config.php
 
-cat <<_EOF_>> /var/www/nextcloud/config/config.php
+cat << _EOF_ >> /var/www/nextcloud/config/config.php
 'memcache.distributed' => '\OC\Memcache\Redis',
 'memcache.local' => '\OC\Memcache\Redis',
 'memcache.locking' => '\OC\Memcache\Redis',
@@ -182,10 +167,9 @@ cat <<_EOF_>> /var/www/nextcloud/config/config.php
      'host' => 'localhost',
      'port' => 6379,
      ),
+);
 _EOF_
 
-sudo systemctl restart apache2 php7.4-fpm
+sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 1024M/g' /etc/php/7.4/fpm/php.ini
 
-sudo sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 1024M/g' /etc/php/7.4/fpm/php.ini
-
-sudo systemctl restart php7.4-fpm
+systemctl restart apache2 php7.4-fpm
